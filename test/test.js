4d44fa1b0d2201960478829c86ed04ef731677fc
Poonam <mni.pora@gmail.com>
2018-10-03 20:19:43 +0200

    boiler plate

---
+++ test/test.js
@@ -0,0 +1,8 @@
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      browser.url("https://www.booli.se/");
+      browser.debug();
+    });
+  });
+});
